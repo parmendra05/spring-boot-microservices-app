71dc0508e6e334b76fa1714fab5cdc7c42e902bd
parmendra kumar <parmendrakumar718@gmail.com>
2023-02-13 10:22:56 +0530

    renamed service registry module

--- service-registry/src/main/java/com/pack/Application.java
+++ service-registry/src/main/java/com/pack/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ConfigServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
